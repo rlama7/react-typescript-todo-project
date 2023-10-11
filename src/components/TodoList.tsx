541724054079b120becd4981a0f8ac61c98cdf9d
Ratna Lama <royalray7@gmail.com>
2023-10-11 13:59:53 -0700

    add TodoList

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import './styles.css';
+import { Todo } from '../model/model';
+import TodoItem from './TodoItem';
 
-const TodoList = () => {
-  return <div>TodoList</div>;
+interface Props {
+  todoList: Todo[];
+  setTodoList: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const TodoList: React.FC<Props> = ({ todoList, setTodoList }) => {
+  return (
+    <div className="todolist">
+      {todoList.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          todoList={todoList}
+          setTodoList={setTodoList}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default TodoList;
