8fde19efd70e2cb40212874fc957cf4a4aa4675b
Ratna Lama <royalray7@gmail.com>
2023-10-11 12:49:01 -0700

    update list

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import { Todo } from './model/model';
+import TodoList from './components/TodoList';
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>('');
+
+  const [todoList, setTodoList] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodoList([...todoList, { id: Date.now(), todo, isDone: false }]);
+      setTodo('');
+    }
+  };
+
+  // console.log(todoList);
+
   return (
     <div className="App">
       <span className="heading">ToDo Task</span>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList />
     </div>
   );
 };
