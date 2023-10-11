2b9011112400e298fd3d9595f567bcce77d8ff42
Ratna Lama <royalray7@gmail.com>
2023-10-11 11:11:47 -0700

    add interface and types

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 // Types
+// Ref: https://www.typescriptlang.org/docs/handbook/2/basic-types.html
 let name: string; // string ONLY type
 let age: number; // number ONLY type
 
@@ -30,7 +31,52 @@ let people: Person[]; // array of person object
 // Function type
 // declare input type and outupt type
 // void signifies it returns nothing.
-let printNameType: (name: string) => void;
+let printNameType: (name: string) => void; // returns undefined
+
+let neverReturn: (name: string) => never; // returns nothing
+
+// any type
+// Not prefered as this defeats the purpose of using Typescript.
+// better option is to use unknown when the type is unknown
+let anyType: any;
+
+let typeUnknown: unknown;
+
+// type are two things: types or alias and interface
+
+interface Data {
+  name: string;
+  age?: number;
+}
+
+// inherit interface use keywords extends
+
+interface Profession extends Data {
+  occupation: string;
+}
+
+type X = {
+  a: string;
+  b: number;
+};
+
+// inherit from X properties
+type Y = X & {
+  c: string;
+  d: number;
+};
+
+let combine: Y = {
+  c: 'comb',
+  d: 1,
+  a: 'Hi',
+  b: 2,
+};
+
+// to extend from interface in type use &
+type Z = Data & {
+  t: string;
+};
 
 function printName(name: string) {
   console.log(name);
